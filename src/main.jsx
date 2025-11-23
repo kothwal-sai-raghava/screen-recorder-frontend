3b3278879ed1149ba654d60d19f44393c2ee8de2
kothwal-sai-raghava <kothwalsairaghava0102@gmail.com>
2025-11-23 20:16:57 +0530

    Add protected routes and handle unauthenticated paths

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,43 @@
-import React from 'react'
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import Home from './components/Home.jsx'
-import Recordings from './components/Recordings.jsx'
+import React from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+
+import App from "./App.jsx";
+import Home from "./components/Home.jsx";
+import Recordings from "./components/Recordings.jsx";
+import ProtectedRoute from "./components/ProtectedRoute.jsx";
 import { Toaster } from "react-hot-toast";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-  <Toaster position="top-right" reverseOrder={false} />
+    <Toaster position="top-right" reverseOrder={false} />
+
     <Routes>
-      <Route path='/' element={<App/>}/>
-      <Route path='/home' element={<Home/>}/>
-      <Route path="/recordings" element={<Recordings/>}/>
+      {/* Public Route → Login/Register */}
+      <Route path="/" element={<App />} />
+
+      {/* Protected Routes */}
+      <Route
+        path="/home"
+        element={
+          <ProtectedRoute>
+            <Home />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/recordings"
+        element={
+          <ProtectedRoute>
+            <Recordings />
+          </ProtectedRoute>
+        }
+      />
+
+      {/* Catch-all → Redirect to Login */}
+      <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   </BrowserRouter>
-)
+);
